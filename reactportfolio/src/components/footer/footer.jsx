d5075ac164db9e4d4823fd0d01aa7b38fba2b9d9
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-04-04 21:26:21 -0400

    setting up header

--- reactportfolio/src/components/footer/footer.jsx
+++ reactportfolio/src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import "./footer.scss"
 
 export default function Footer() {
     return(
-        <div className="footer">
+        <div className="footer" id="footer">
 
 
         </div>
--- reactportfolio/src/components/header/header.jsx
+++ reactportfolio/src/components/header/header.jsx
@@ -4,13 +4,16 @@ export default function Header() {
     return(
         <div className="header">
             <div className="wrapper">
-<div className="left">
+                <div className="left">
+                    <a href="#intro" className="logo"> Savannah Clarke</a>
+                </div>
+                    <div className="right">
+                        <div className="hamburger"></div>
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
 
-</div>
-<div className="right">
-
-    
-</div>
+                </div>
             </div>
 
         </div>
