0ff3cd26019e13dcbd81dc58c027b0d12da00272
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2022-04-06 01:27:41 -0400

    contact form

--- reactportfolio/src/components/contact/contact.jsx
+++ reactportfolio/src/components/contact/contact.jsx
@@ -4,8 +4,17 @@ import "./contact.scss"
 export default function Contact() {
     return(
         <div className="contact" id="contact">
-jv nb
-
+<div className="left">
+    <img src="assets/" alt="" />
+</div>
+<div className="right">
+    <h2>Contact</h2>
+    <form>
+        <input type="text" placeholder="Email"/>
+        <textarea placeholder="Message"></textarea>
+        <button type="submit">Send</button>
+    </form>
+</div>
         </div>
 
     )
